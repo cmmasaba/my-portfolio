a0749a3fa3c9a43ceb4f86f562d5f1f9c524778d
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-23 11:20:25 +0000

    Customized the about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,28 +15,29 @@ import React from "react";
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
+ * import image from "../images/motion-background.jpg";
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/cubes.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "different colors cubes background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Telecommunications Engineering student studying at Jomo Kenyatta University of Agriculture and Technology. I am passionate about technology and this drove me start learning programming as a hobby. I developed deep interest in software design and development and I have been learning and practicing ever since.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python web frameworks i.e. Django",
+  "Web design and development",
+  "Tasks automation using Python",
+  "No Code development using AppSheet",
+  "Data structures and algorithms",
+  "Cloud computing",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation and make a positive impact on the world around me. I like staying up to date on the latest trends in technology and I am always looking for opportunities to learn and grow. I have two years experience developing software in production, I have gained a huge amount of experience in this time which I am eager to apply as I continue my career..";
 
 const About = () => {
   return (
